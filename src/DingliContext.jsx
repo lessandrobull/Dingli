440bc4b0b7f793496c9d3d0ecec683487f57b5f3
lessandrobull <lessandro.bull@gmail.com>
2026-09-14 03:35:04 -0300

    Update DingliContext.jsx

--- src/DingliContext.jsx
+++ src/DingliContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useMemo } from 'react';
-import { temas, interfaceTraducoes } from './constant';
+import { temas, interfaceTraducoes } from './constant'; 
 
 const DingliContext = createContext();
 
